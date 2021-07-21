87ad3c96e3b5a01219afb776fb8a4f59d5c372f9
Justin <justinkollie890@yahoo.com>
2021-07-21 01:51:52 -0400

    Changes made

---
+++ .history/src/components/Header_20210721012204.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Search from './Search';
+import {Link} from 'react-router-dom';
+import {RiMenu2Line} from 'react-icons/ri';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="respNav">
+                <h3>iTheater</h3>
+                <div className="resp_links">
+                    <Link to='/'>Home</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
+            </div>
+            <div className="my_container">
+                <span className="dummyElement">
+                    <RiMenu2Line />
+                </span>
+                <Search />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
