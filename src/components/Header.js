62ace86eb632a084036c52574a7f4ed998fd4c20
just-mann <justinkollie890@yahoo.com>
2021-09-09 18:36:34 -0700

    Changes made

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,33 @@ import React from 'react';
 import Search from './Search';
 import {NavLink} from 'react-router-dom';
 import {RiMenu2Line} from 'react-icons/ri';
+import $ from 'jquery';
 
 const Header = () => {
+    $(() => {
+        const hamburger = $('#hamburger');
+        const closeBtn = $('#closeBtn');
+        const sideNav = $('#sideNav');
+
+
+        
+        const showSideNav = () => {
+            sideNav.show();
+        }
+        
+        const hideSideNav = () => {
+            sideNav.hide();
+        }
+
+        hamburger.on('click', showSideNav);
+        closeBtn.on('click', hideSideNav);
+    });
+
     return (
         <div className="header">
             <div className="my_container">
                 <NavLink className="app_Logo" to='/'>iTheater</NavLink>
-                <RiMenu2Line className="hamburger"/>
+                <RiMenu2Line className="hamburger" id="hamburger" />
                 <Search />
             </div>
         </div>
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import {BsArrowRight} from 'react-icons/bs';
+import {VscChromeClose} from 'react-icons/vsc';
 
 const SideNav = () => {
     return (
-        <div className="sideNav">
+        <div className="sideNav" id="sideNav">
+            <VscChromeClose className="closeNav" id="closeBtn" />
             <div className="links">
-                <NavLink to='/'  activeClassName="active">Home <BsArrowRight className="arrows"/></NavLink>
+                <NavLink to='/' activeClassName="active">Home <BsArrowRight className="arrows"/></NavLink>
                 <NavLink to="/contact" activeClassName="active">Contact <BsArrowRight className="arrows"/></NavLink>
             </div>
         </div>
